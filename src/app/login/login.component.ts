99a39a25cbf9cf02efa348dddba9a51731b81aa5
JohannaKrennhuber <S1910238039@students.fh-hagenberg.at>
2021-11-19 15:01:19 +0100

    Send help cuz this thing is a hopeless case

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,36 +1,67 @@
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { Login } from '../../models/auth/login';
-import { AuthenticationService } from '../../services/authentication.service';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+import { ReactiveFormsModule } from '@angular/forms';
+
+//import { AccountService, AlertService } from '@app/_services';
 
 @Component({
   selector: 'app-login',
-  templateUrl: 'login.component.html',
+  templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
-  login: Login = { username: '', password: '' };
-  submitted = false;
+export class LoginComponent implements OnInit {
+    yform!: FormGroup;
+    loading = false;
+    submitted = false;
 
-  constructor(
-    public authService: AuthenticationService,
-  ) { }
+    constructor(
+        //public form: null,
+        private formBuilder: FormBuilder,
+        private route: ActivatedRoute,
+        private router: Router,
+        //private accountService: AccountService,
+        //private alertService: AlertService
+    ) { }
 
-  // async onLogin(form: NgForm) {
-  //   this.submitted = true;
+    ngOnInit() {
+        this.yform = this.formBuilder.group({
+            username: ['', Validators.required, Validators.pattern(' ^[a-z0-9_-]$ '), Validators.minLength(8), Validators.maxLength(21)],
+            password: ['', Validators.required, Validators.pattern(' ^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).$ '), 
+            Validators.minLength(8), Validators.maxLength(21)]
+        });
+    }
 
-  //   if (form.valid) {
-  //       this.authService.login(this.login);
-  //   }
-  // }
+    // convenience getter for easy access to form fields
+    get f() { return this.yform.controls; }
 
-  async onLogin() {
-    this.submitted = true;
+    onSubmit() {
+        //this.submitted = true;
+        console.log(this.yform.value);
 
-    this.authService.login(this.login);
-  }
+        // reset alerts on submit
+       // this.alertService.clear();
 
-  onSignup() {
-    // this.router.navigateByUrl('/signup');
-  }
+        // stop here if form is invalid
+        if (this.yform.invalid) {
+          console.log("triggered");
+            //return;
+        }
+
+        this.loading = true;
+        /*this.accountService.login(this.f.username.value, this.f.password.value)
+            .pipe(first())
+            .subscribe({
+                next: () => {
+                    // get return url from query parameters or default to home page
+                    const returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+                    this.router.navigateByUrl(returnUrl);
+                },
+                error: error => {
+                    this.alertService.error(error);
+                    this.loading = false;
+                }
+            });*/
+    }
 }
