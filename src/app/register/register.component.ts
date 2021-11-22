6ec192c79b8288f1370654025d359b7006ca5340
Phumelele Mthimunye <mrgeminiaura@gmail.com>
2021-11-22 13:03:49 +0200

    reg

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -21,8 +21,8 @@ submitted:boolean = false;
     this.registerForm=this.formBuilder.group({
       username: new FormControl(null,[Validators.required,Validators.maxLength(21),Validators.pattern('^[A-Za-z]+$')]),
       password: new FormControl(null,[Validators.required,Validators.minLength(6),Validators.maxLength(21)]),
-      confirmpassword: new FormControl(null,[Validators.required,Validators.maxLength(21),]),
-      email:new FormControl(null,[Validators.required,Validators.maxLength(40),Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")])
+      confirmpassword: new FormControl(null,[Validators.required,Validators.maxLength(21)]),
+      email:new FormControl(null,[Validators.required,Validators.maxLength(40),Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')])
      
     
     } ,
