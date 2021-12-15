72fcabd26785633205fe45d6991bd860df840978
Tshepi21 <tshepisomohatlane1@gmail.com>
2021-12-15 06:05:14 -0800

    implement login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,6 +7,7 @@ import { Login } from 'src/models/auth/login';
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
+  template:''
 })
 export class LoginComponent implements OnInit {
     lgnForm!: FormGroup;
